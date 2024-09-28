5d858c72016a246d51e0b1cb4e10998fd1c8b124
Alexey Buravov <aburavov189@gmail.com>
2024-09-29 02:17:15 +0500

    step 2

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -4,5 +4,17 @@ require_relative "hexlet_code/version"
 
 module HexletCode
   class Error < StandardError; end
-  # Your code goes here...
+
+  # Tag - builds an HTML tag
+  class Tag
+    def self.build(tag_name, attrs = {})
+      attrs_strs = []
+      attrs.each do |key, value|
+        attrs_strs << "#{key}=\"#{value}\""
+      end
+      return "<#{tag_name} #{attrs_strs.join(" ")}>" if attrs_strs.any?
+
+      "<#{tag_name}>"
+    end
+  end
 end
--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -3,11 +3,13 @@
 require "test_helper"
 
 class TestHexletCode < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::HexletCode::VERSION
+  def test_br
+    result = HexletCode::Tag.build("br")
+    assert_equal "<br>", result
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_input
+    result = HexletCode::Tag.build("input", type: "submit", value: "Save")
+    assert_equal '<input type="submit" value="Save">', result
   end
 end
