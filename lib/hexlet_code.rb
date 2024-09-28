dcbb2bb243017418d15891d2f728c4d975903ff2
Alexey Buravov <aburavov189@gmail.com>
2024-09-29 03:12:13 +0500

    step 4

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -2,8 +2,14 @@
 
 require_relative "hexlet_code/version"
 
+# HexletCode - main module for the form generator
 module HexletCode
   class Error < StandardError; end
+
+  def self.form_for(ropo, url = "")
+    action = url.empty? ? "#" : url
+    Tag.build("form", action: action, method: "post")
+  end
 
   # Tag - builds an HTML tag
   class Tag
@@ -12,9 +18,18 @@ module HexletCode
       attrs.each do |key, value|
         attrs_strs << "#{key}=\"#{value}\""
       end
-      return "<#{tag_name} #{attrs_strs.join(" ")}>" if attrs_strs.any?
 
-      "<#{tag_name}>"
+      attrs_content = attrs_strs.any? ? " #{attrs_strs.join(" ")}" : ""
+
+      content = block_given? ? yield : ""
+
+      return "<#{tag_name}#{attrs_content}>" if single_tag?(tag_name)
+
+      "<#{tag_name}#{attrs_content}>#{content}</#{tag_name}>"
+    end
+
+    def self.single_tag?(tag_name)
+      %w[br img input].include? tag_name
     end
   end
 end
--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -3,6 +3,18 @@
 require "test_helper"
 
 class TestHexletCode < Minitest::Test
+  def test_form
+    result = HexletCode.form_for(nil)
+    assert_equal "<form action=\"#\" method=\"post\"></form>", result
+  end
+
+  def test_form_with_url
+    result = HexletCode.form_for(nil, "/users")
+    assert_equal "<form action=\"/users\" method=\"post\"></form>", result
+  end
+end
+
+class TestTag < Minitest::Test
   def test_br
     result = HexletCode::Tag.build("br")
     assert_equal "<br>", result
@@ -11,5 +23,15 @@ class TestHexletCode < Minitest::Test
   def test_input
     result = HexletCode::Tag.build("input", type: "submit", value: "Save")
     assert_equal '<input type="submit" value="Save">', result
+  end
+
+  def test_label
+    result = HexletCode::Tag.build("label", for: "email") { "Email" }
+    assert_equal '<label for="email">Email</label>', result
+  end
+
+  def test_label_without_attrs
+    result = HexletCode::Tag.build("label") { "Email" }
+    assert_equal "<label>Email</label>", result
   end
 end
